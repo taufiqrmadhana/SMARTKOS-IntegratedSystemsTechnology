e0b3d213920d09cb7680f7ea679338ea5649334a
taufiqrmadhana <149040722+taufiqrmadhana@users.noreply.github.com>
2025-01-10 14:26:45 +0700

    fully using external api (integrated)

--- Integrated/app/Config/Routes.php
+++ Integrated/app/Config/Routes.php
@@ -32,26 +32,3 @@ $routes->group('auth', ['namespace' => 'App\Controllers'], function($routes) {
     $routes->post('logout', 'AuthController::logout');     // POST /auth/logout
 });
 
-// Terapkan filter 'auth' untuk grup maintenance
-$routes->group('maintenance', [
-    'namespace' => 'App\Controllers',
-    'filter'    => 'auth'
-], function($routes) {
-    $routes->post('create', 'MaintenanceController::create');        // POST /maintenance/create
-    $routes->get('/', 'MaintenanceController::index');               // GET /maintenance/
-    $routes->put('update/(:num)', 'MaintenanceController::update/$1'); // PUT /maintenance/update/{id}
-    $routes->get('filter', 'MaintenanceController::filter');         // GET /maintenance/filter
-    $routes->get('stats', 'MaintenanceController::stats');           // GET /maintenance/stats
-    $routes->delete('delete/(:num)', 'MaintenanceController::delete/$1'); // DELETE /maintenance/delete/{id}
-});
-
-// Rute untuk laporan (reports)
-$routes->group('report', function ($routes) {
-    $routes->get('/', 'ReportController::index');              // Mendapatkan semua laporan
-    $routes->get('status/(:any)', 'ReportController::getByStatus/$1'); // Mendapatkan laporan berdasarkan status
-    $routes->post('create', 'ReportController::create');       // Membuat laporan baru
-    $routes->delete('delete/(:num)', 'ReportController::delete/$1'); // Menghapus laporan berdasarkan ID
-});
-
-
-
